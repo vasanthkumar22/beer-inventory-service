c76db325943c43837e6360358649c9994db2c60a
vasanths <sreeranga>
2020-09-10 14:39:27 +0530

    Initial Project

---
+++ src/test/java/com/learning/beerinventoryservice/BeerInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.beerinventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerInventoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
